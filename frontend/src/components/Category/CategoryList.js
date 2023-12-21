0b11038b0ed8f6472c72d1ffa5d121d9c2f868f1
MichaelFJ98 <56739386+MichaelFJ98@users.noreply.github.com>
2023-12-21 18:39:15 +0100

    Added Categoryrow, categorylist and navbar components (unfinished)

---
+++ frontend/src/components/Category/CategoryList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CategoryList(){
+    return(
+        <div className="flex flex-col ">
+            
+                
+        </div>
+    )
+}
---
+++ frontend/src/components/Category/CategoryRow.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function CategoryRow(){
+    return(
+        <div className="flex m-2 p-2 w-screen h-80 rounded-lg border-2 border-black font-bold flex-col ">
+            <h1 className="text-black text-2xl bg-blue-200 h-min">Testname</h1>
+            <div className="h-full bg-red-200">
+                dzaazd
+            </div>
+                
+        </div>
+    )
+}
