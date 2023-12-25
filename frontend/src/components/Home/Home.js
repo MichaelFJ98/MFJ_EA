17cc932c7885be309ce9309de6c4a928ff07fc48
MichaelFJ98 <56739386+MichaelFJ98@users.noreply.github.com>
2023-12-25 14:07:02 +0100

    Refactored code to work with react-router-dom setup

---
+++ frontend/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import CategoryList from '../Category/CategoryList.js';
+
+function Home() {
+  return (
+    <div className="bg-white">
+      <div className="flex">
+        <CategoryList/>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
